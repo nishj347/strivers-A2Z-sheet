8b0889fe48191553ab1c8d35341b651c1d31d620
nish <144939153+nishj347@users.noreply.github.com>
2024-08-31 08:55:01 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -1,8 +1,19 @@
 // Author : nishj347@ github
-
+// pattern 1
 void nForest(int n) {
 	for (int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
+
+
+// pattern2
+void nForest(int n) {
+	for (int i=0;i<n;i++){
+		for (int j=0;j<=i;j++){
 			cout<<"* ";
 		}
 		cout<<endl;
