750e1d6efcdb7fe57aa456354c50c06561bac211
nish <144939153+nishj347@users.noreply.github.com>
2024-08-31 08:50:21 +0530

    lec1

--- basic.cpp
+++ basic.cpp
@@ -31,3 +31,15 @@ int main(){
 
     return 0;
 }
+
+
+->  #include<iostream>
+using namespace std;
+
+int main(){
+    string s="striver";
+    int len=s.size();
+    cout<<s[len-1];
+    
+    return 0;
+}
