e26c60d3c1c3c891ce94d3f174c3607d53511007
nish <144939153+nishj347@users.noreply.github.com>
2024-08-31 08:59:39 +0530

    lec2

--- patterns.cpp
+++ patterns.cpp
@@ -98,7 +98,35 @@ void nStarTriangle(int n) {
   }
 }
 
-pattern 9
+// pattern 9
+void nStarDiamond(int n) {
+    for (int i=0;i<n;i++){
+      for (int j=0;j<n-i-1;j++){
+          cout<<" ";
+      }
+       for (int j=0;j<2*i+1;j++){
+           cout<<"*";
 
+       }
+        for (int j=0;j<n-i-1;j++){
+            cout<<" ";
+
+        }
+          cout<<endl;
+  }
+      for (int i=0;i<n;i++){
+      for (int j=0;j<i;j++){
+          cout<<" ";
+      }
+       for (int j=0;j<2*n-(2*i+1);j++){
+           cout<<"*";
+
+       }
+       for (int j = 0; j < i; j++) {
+           cout << " ";
+       }
+       cout << endl;
+      }
+}
 
 
