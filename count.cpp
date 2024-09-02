3910e3b67c2145b9de7199e7692ba127ae81625f
nish <144939153+nishj347@users.noreply.github.com>
2024-09-02 20:22:26 +0530

    Update count.cpp

--- count.cpp
+++ count.cpp
@@ -37,3 +37,37 @@ int main(){
 
 // int cnt=(int)(log10(n)+1);
 // return cnt;
+
+
+// reverseno:
+// Example 1:
+// Input: N = 123
+// Output: 321
+// Explanation: The reverse of 123 is 321
+
+// Example 2:
+// Input: N = 234
+// Output: 432
+// Explanation: The reverse of 234 is 432
+
+#include<bits/stdc++.h>
+using namespace std;
+int reverseno(int n){
+    int reverse=0;
+    while(n>0){
+        int lastdigit= n % 10;
+        int reverse=reverse*10+lastdigit;
+        n=n/10;
+        
+    }
+    return reverse;
+}
+int main(){
+    int n;
+    cout<<"eneter the nummber:";
+    cin>>n;
+    int result=reverseno(n);
+    cout<<result;
+    return 0;
+
+}
