686419efe4c923e942f166b8bc805e6c98d794ba
nish <144939153+nishj347@users.noreply.github.com>
2024-09-02 20:23:43 +0530

    Update count.cpp

--- count.cpp
+++ count.cpp
@@ -71,3 +71,42 @@ int main(){
     return 0;
 
 }
+
+// print division:
+#include<iostream>
+using namespace std;
+
+int divisor(int n){
+    for (int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" "<<endl;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the number: "<<endl;
+    cin>>n;
+    divisor(n);
+    return 0;
+}
+// prime 
+#include<iostream>
+using namespace std;
+
+int divisor(int n){
+    for (int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" "<<endl;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the number: "<<endl;
+    cin>>n;
+    divisor(n);
+    return 0;
+}
