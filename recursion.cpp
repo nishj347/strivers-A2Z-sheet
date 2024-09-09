2c43a2bf1079bb0d070c923e570693d44504a93a
nish <144939153+nishj347@users.noreply.github.com>
2024-09-09 15:18:06 +0530

    lec5

--- recursion.cpp
+++ recursion.cpp
@@ -185,6 +185,38 @@ int main() {
     return 0;
 }
 
+// fiboanacci
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1){
+    return n;
+    }
+    // if(n==0){
+    //     return 0;
+    // }
+    // if(n==1){
+    //     return 1;
+    // }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    cout<<"fibonacci series:"<<endl;
+    // for(int i=0;i<n;i++){
+        cout<<fibonacci(n)<<endl;
+    // }
+        return 0;
+    
+
+}
+
 
 
 
